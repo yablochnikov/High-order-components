3b3d8685f20fd8b8d17dc99ad8e769436a77a5ec
Nikita Yablochnikov <yablochnikovnikita@gmail.com>
2022-08-24 18:32:36 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import { Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+const getDataFromFirstFetch = () => {
+  return 10;
+};
+const getDataFromSecondFetch = () => {
+  return 20;
+};
+
+const SliderFirst = () => {
+  const [slide, setSlide] = useState(0);
+
+  useEffect(() => {
+    setSlide(getDataFromFirstFetch());
+  }, []);
+
+  function changeSlide(i) {
+    setSlide(slide => slide + i);
+  }
+
+  return (
+    <Container>
+      <div className='slider w-50 m-auto'>
+        <img
+          className='d-block w-100'
+          src='https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg'
+          alt='slide'
+        />
+        <div className='text-center mt-5'>Active slide {slide}</div>
+        <div className='buttons mt-3'>
+          <button
+            className='btn btn-primary me-2'
+            onClick={() => changeSlide(-1)}
+          >
+            -1
+          </button>
+          <button
+            className='btn btn-primary me-2'
+            onClick={() => changeSlide(1)}
+          >
+            +1
+          </button>
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+const SliderSecond = () => {
+  const [slide, setSlide] = useState(0);
+  const [autoplay, setAutoplay] = useState(false);
+
+  useEffect(() => {
+    setSlide(getDataFromSecondFetch());
+  }, []);
+
+  function changeSlide(i) {
+    setSlide(slide => slide + i);
+  }
+
+  return (
+    <Container>
+      <div className='slider w-50 m-auto'>
+        <img
+          className='d-block w-100'
+          src='https://www.planetware.com/wpimages/2020/02/france-in-pictures-beautiful-places-to-photograph-eiffel-tower.jpg'
+          alt='slide'
+        />
+        <div className='text-center mt-5'>
+          Active slide {slide} <br />
+          {autoplay ? "auto" : null}{" "}
+        </div>
+        <div className='buttons mt-3'>
+          <button
+            className='btn btn-primary me-2'
+            onClick={() => changeSlide(-1)}
+          >
+            -1
+          </button>
+          <button
+            className='btn btn-primary me-2'
+            onClick={() => changeSlide(1)}
+          >
+            +1
+          </button>
+          <button
+            className='btn btn-primary me-2'
+            onClick={() => setAutoplay(autoplay => !autoplay)}
+          >
+            toggle autoplay
+          </button>
+        </div>
+      </div>
+    </Container>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <SliderFirst />
+      <SliderSecond />
+    </>
   );
 }
 
